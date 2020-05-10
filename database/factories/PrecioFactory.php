c0ebe368c93417a68dc6c567f205763473b886a8
Eduardo Tucto <tuctoeduardo@gmail.com>
2020-05-09 21:26:53 -0500

    Se crea modelo,migracion,controlador y seeders de las tablas(make:model -all)

---
+++ database/factories/PrecioFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Precio;
+use Faker\Generator as Faker;
+
+$factory->define(Precio::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
